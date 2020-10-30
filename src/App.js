21c7205de3d77350e386c88b036c6d1ec131265a
pamsflying <pam.miller61@gmail.com>
2020-10-30 13:39:32 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import "./App.css";
 export default function App() {
     return (
         <div className="Weather">
-        <div className="App">
-          <Search />
-          <Weather />
-        </div>
+            <div className="App">
+                <div className="container">
+                    <Search />
+                    <Weather />
+                </div>
  
-        <footer>
-        <a href="https://github.com/pamsflying/react-weather-app.git" target="_blank" rel="noreferrer"> Open-source code</a> by Pam Miller. Hosted by <a href=
-        "https://peaceful-boyd-df51b2.netlify.app" target="_blank" rel="noreferrer"> Netlify</a>.
-        </footer>
-
+                <footer>
+                    <a href="https://github.com/pamsflying/react-weather-app.git" target="_blank" rel="noreferrer"> Open-source code</a> by Pam Miller. Hosted by <a href=
+                    "https://peaceful-boyd-df51b2.netlify.app" target="_blank" rel="noreferrer"> Netlify</a>.
+                </footer>
+        </div>
      </div>
     );
   }
